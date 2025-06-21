9236942d6259b513de4d945ac1708d9c7f1dca22
Vitalii <vitalik.barabash.2002@gmail.com>
2025-06-21 22:49:09 +0300

    defined instances of options

--- CarInsuranceSalesBot/Program.cs
+++ CarInsuranceSalesBot/Program.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using CarInsuranceSalesBot.Options;
+
+using Microsoft.Extensions.Configuration;
 
 // configuration handler setup
 IConfiguration configuration =
     new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .SetBasePath(basePath: Directory.GetCurrentDirectory())
+        .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
         .Build();
+
+// setup services options
+TelegramBotOptions telegramBotOptions =
+    new()
+    {
+        BotToken =
+            configuration["TelegramBotToken"] ??
+            throw new InvalidOperationException(message: "Telegram bot token is missing")
+    };
+MindeeOptions mindeeOptions =
+    new()
+    {
+        ApiKey =
+            configuration["MindeeOptions:ApiKey"] ??
+            throw new InvalidOperationException(message: "Mindee API key is missing")
+    };
+    
