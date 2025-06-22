e809fed00b54802a7556b8725074d8b221ca99df
Vitalii <vitalik.barabash.2002@gmail.com>
2025-06-22 12:51:50 +0300

    defined user sessions class

---
+++ CarInsuranceSalesBot/Models/UserSession.cs
@@ -0,0 +1,14 @@
+namespace CarInsuranceSalesBot.Models;
+
+public class UserSession(long userId)
+{
+    public long UserId { get; init; } = userId;
+
+    public MemoryStream PassportImageStream { get; set; } = null!;
+
+    public MemoryStream VehicleIdImageStream { get; set; } = null!;
+
+    public MindeeDataExtractionResponse MindeeDataExtractionResponse { get; set; } = null!;
+
+    public required int Step { get; set; }
+}
