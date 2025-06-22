66f8e46eed2eed8e27cd26cb2ac2c8363721dd94
Vitalii <vitalik.barabash.2002@gmail.com>
2025-06-22 12:55:00 +0300

    defined user session managing service

---
+++ CarInsuranceSalesBot/Services/UserSessionManager.cs
@@ -0,0 +1,21 @@
+﻿using CarInsuranceSalesBot.Models;
+
+namespace CarInsuranceSalesBot.Services;
+
+public class UserSessionManager
+{
+    private readonly Dictionary<long, UserSession> _sessions = new();
+
+    public UserSession GetOrCreateUserSession(long userId)
+    {
+        if (_sessions.TryGetValue(key: userId, value: out UserSession? session))
+        {
+            return session;
+        }
+
+        session = new UserSession(userId) { Step = 0 };
+        _sessions[userId] = session;
+
+        return session;
+    }
+}
