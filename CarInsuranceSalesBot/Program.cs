b69491aaf06433a1d1fbf1c4545b368d73a81832
Vitalii <vitalik.barabash.2002@gmail.com>
2025-06-24 00:02:35 +0300

    added services registration and bot boot up

--- CarInsuranceSalesBot/Program.cs
+++ CarInsuranceSalesBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CarInsuranceSalesBot.Options;
+using CarInsuranceSalesBot.Services;
 
 using Microsoft.Extensions.Configuration;
 
@@ -24,4 +25,23 @@ MindeeOptions mindeeOptions =
             configuration["MindeeOptions:ApiKey"] ??
             throw new InvalidOperationException(message: "Mindee API key is missing")
     };
-    
+
+// service registration
+CancellationTokenSource cts = new();
+
+UserSessionManager sessionManager = new();
+MindeeOcrService ocrService = new(mindeeOptions);
+PdfPolicyGenerationService pdfPolicyGenerationService = new();
+
+BotService service =
+    new(
+        telegramBotOptions,
+        sessionManager,
+        ocrService,
+        pdfPolicyGenerationService,
+        cancellationToken: cts.Token);
+
+// start application core service
+await service.StartAsync();
+
+Console.ReadLine();
